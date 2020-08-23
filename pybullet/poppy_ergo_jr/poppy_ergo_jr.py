736239f3b8c04293188c51f8ace0378a383af95e
garrettkatz <garrett.katz@gmail.com>
2020-08-23 16:50:47 -0400

    started pybullet with poppy ergo jr

---
+++ pybullet/poppy_ergo_jr/poppy_ergo_jr.py
@@ -0,0 +1,23 @@
+import pybullet as p
+import time
+import numpy as np
+import pybullet_data
+
+physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
+p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+p.setGravity(0,0,-10)
+planeId = p.loadURDF("plane.urdf")
+cubeStartPos = [0,0,1]
+cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
+boxId = p.loadURDF("poppy_ergo_jr.urdf",cubeStartPos, cubeStartOrientation, globalScaling=0.01)
+for i in range (10000):
+    p.setJointMotorControlArray(boxId, np.arange(6), p.POSITION_CONTROL,
+        targetPositions=np.ones(6))
+    p.stepSimulation()
+    time.sleep(1./240.)
+cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
+print(cubePos,cubeOrn)
+# print("joints",p.getNumJoints(boxId)) # 6
+p.disconnect()
+
+
