f5a93b47392e16e9a6247e1ca85f36b40ea6554c
garrettkatz <garrett.katz@gmail.com>
2021-01-06 16:39:06 -0500

    started walking angles

--- scripts/launch.py
+++ scripts/launch.py
@@ -1,12 +1,26 @@
 from pypot.creatures import PoppyHumanoid as PH
 from pypot.sensor import OpenCVCamera
 import time
+import pickle as pk
 import numpy as np
 import matplotlib.pyplot as plt
 from record_angles import hotkeys
 
 p = PH()
 c = OpenCVCamera("poppy-cam",0,10)
+
+def angs():
+    return {m.name: m.present_position for m in p.motors}
+
+def go(angs):
+    p.goto_position(angs, 3, wait=True)
+
+def save(angs, name):
+    with open(name, "wb") as f: pk.dump(angs, f)
+
+def toggle(motors):
+    if type(motors) is not list: motors = [motors]
+    for m in motors: m.compliant = not m.compliant
 
 print("Created poppy humanoid p and opencvcamera c with 10 fps.  Don't forget to p.close() and c.close() before quit() when you are finished to clean up the motor state.")
 
