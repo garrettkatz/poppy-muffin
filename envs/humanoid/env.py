fe2c01c9cbdcfdcff2b5f772e47253ec934d936a
garrettkatz <garrett.katz@gmail.com>
2021-05-14 23:25:30 -0400

    crawl working in simulation (in poppy humanoid env main)

--- envs/humanoid/env.py
+++ envs/humanoid/env.py
@@ -49,11 +49,22 @@ class PoppyHumanoidEnv(object):
         trajectory = weights * target + (1 - weights) * current
         for action in trajectory: self.step(action)
             
+def clean_angles(angles):
+    # transforms angles measured on real poppy for pybullet
+    # angles should be {name: degrees} dictionary
+    # shoulder_x off by 90 degrees
+    # also, required lower/uppers switched and negated in urdf,
+    # except for preserved joints below
+    # all other joint angles need to be negated
+    # all angles also converted to radians
 
-def next_position(current, target, speed, timestep):
-    v = target - current
-    v *= speed / np.sum(v**2)**.5
-    return current + v * timestep
+    preserved = ["l_hip_y", "l_ankle_y", "l_shoulder_y", "l_elbow_y"]
+    cleaned = {}
+    for m,p in angles.items():
+        if m[2:] == 'shoulder_x': p -= 90
+        if m not in preserved: p = -p
+        cleaned[m] = p * np.pi / 180
+    return cleaned
 
 if __name__ == '__main__':
 
@@ -80,44 +91,59 @@ if __name__ == '__main__':
         'r_hip_z':-10., 'l_hip_z':10.,
         'r_hip_x':0., 'l_hip_x':0.,})
     
-    # pybullet specific:
-    crawl_angles.update({
-        'r_shoulder_x': -90, 'l_shoulder_x': -90,
-        'r_shoulder_y': 45, 'l_shoulder_y': 45,})
+    # pybullet specific (not compliant):
+    crawl_angles.update({'r_shoulder_y': 45, 'l_shoulder_y': 45,})
     
-    flips = ["l_hip_y", "l_ankle_y", "l_shoulder_y", "l_elbow_y"]
+    angles = [crawl_angles]
     
-    for m,p in crawl_angles.items():
-        s = 1 if m in flips else -1
-        pb.resetJointState(env.robot_id, env.joint_index[m], s * p * np.pi / 180)
+    angle_updates = [
+        {'l_shoulder_x': -13., 'l_hip_z': 20., 'r_hip_z':-20., 'bust_x':-10.}, # lean left
+        {'r_shoulder_x':12., 'abs_x': -3., 'abs_y': 37., 'abs_z': -15., 'r_elbow_y':-90.}, # rotate torso
+        {'l_elbow_y':-90., 'abs_z': 0., 'l_shoulder_y': 0}, # left elbow perp
+        {'l_shoulder_x':0., 'r_shoulder_x':0.},
+        {'r_elbow_y': -115., 'l_elbow_y': -115.,'l_hip_y': -70., 'l_knee_y': 100.,'r_hip_y': -70., 'r_knee_y': 100., 'l_shoulder_y': 45}, # forward thighs vertical
+        {'r_hip_z': 5., 'r_hip_y': -95., 'l_hip_y': -95., 'l_hip_z': 18., 'abs_z': 18., 'l_knee_y': 100., 'r_knee_y': 100.}, # hip raised
+        {'r_hip_z': -2., 'r_hip_y': -100., 'l_hip_y': -70., 'l_hip_z': 2., 'abs_z': -15., 'l_knee_y': 100., 'r_knee_y': 125.}, # right leg forward
+        {'r_hip_z': 0., 'r_hip_y': -100., 'l_hip_y': -100., 'l_hip_z': 0., 'abs_z': 0., 'l_knee_y': 125., 'r_knee_y': 125.}, # left leg forward
+    ]
+    
+    for changes in angle_updates:
+        angles.append(dict(angles[-1]))
+        angles[-1].update(changes)
+        
+    waypoints = np.zeros((len(angles), N))
+    for a,angle in enumerate(angles):
+        cleaned = clean_angles(angle)
+        for m,p in cleaned.items():
+            waypoints[a, env.joint_index[m]] = p
+
+    # initial angles/position
+    for p, position in enumerate(waypoints[-1]):
+        pb.resetJointState(env.robot_id, p, position)
     pb.resetBasePositionAndOrientation(env.robot_id,
         (0, 0, .3),
         pb.getQuaternionFromEuler((.25*np.pi,0,0)))
-    
-    waypoints = np.zeros((len(angles), N))
-    for a,angle in enumerate(angles):
-        for m,p in angle.items():
-            waypoints[a, env.joint_index[m]] = p * np.pi / 180
     
     # waypoints = np.array([
     #     [1. if i == env.joint_index["r_shoulder_x"] else 0. for i in range(N)],
     #     ])
     
-    # while True:
 
-    #     target = waypoints[0]
-    #     duration = 1
-    #     env.goto_position(target, duration)
-    #     input("done...")
-    #     waypoints = waypoints[1:]
-    #     if len(waypoints) == 0: break
-    
-    input('.')
-    action = env.current_position()
-    j = env.joint_index["r_hip_y"]
+    input("ready...")
+    w = 0
     while True:
-        env.step(action)
-        # print(action[j])
-        # print(env.current_position()[j])
-        # input('.')
+
+        target = waypoints[w]
+        duration = .25
+        env.goto_position(target, duration)
+        w = (w + 1) % len(waypoints)
+    
+    # input('.')
+    # action = env.current_position()
+    # j = env.joint_index["l_elbow_y"]
+    # while True:
+    #     env.step(action)
+    #     # print(action[j])
+    #     # print(env.current_position()[j])
+    #     # input('.')
 
