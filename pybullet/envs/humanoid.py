fixed upper/lower limits for changed rotation axes in poppy_humanoid.pybullet.urdf

pybullet/envs/humanoid.py
@@ -47,29 +47,27 @@ class PoppyHumanoidEnv(object):
     def current_position(self):
         states = pb.getJointStates(self.robot_id, range(len(self.joint_index)))
         return np.array([state[0] for state in states])
+    
+    def set_position(self, position):
+        # position: np.array
+        for p, angle in enumerate(position):
+            pb.resetJointState(self.robot_id, p, angle)
 
-    def goto_position(self, target, duration):
+    def goto_position(self, target, duration, hang=False):
         current = self.current_position()
         num_steps = int(duration / self.timestep + 1)
         weights = np.linspace(0, 1, num_steps).reshape(-1,1)
         trajectory = weights * target + (1 - weights) * current
-        for action in trajectory: self.step(action)
+        for action in trajectory:
+            self.step(action)
+            if hang: input('..')
             
-def clean_angles(angles):
-    # transforms angles measured on real poppy for pybullet
-    # angles should be {name: degrees} dictionary
-    # shoulder_x off by 90 degrees
-    # also, required lower/uppers switched and negated in urdf,
-    # except for preserved joints below
-    # all other joint angles need to be negated
-    # all angles also converted to radians
-
-    preserved = ["l_hip_y", "l_ankle_y", "l_shoulder_y", "l_elbow_y"]
-    preserved.extend(['abs_x', 'bust_x'])
+# convert from physical robot angles to pybullet angles
+# degrees are converted to radians
+# other conversions are automatic from poppy_humanoid.pybullet.urdf
+def convert_angles(angles):
     cleaned = {}
     for m,p in angles.items():
-        if m[2:] == 'shoulder_x': p -= 90
-        if m not in preserved: p = -p
         cleaned[m] = p * np.pi / 180
     return cleaned
 

pybullet/tasks/check/check.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append('../../envs')
 
 import pybullet as pb
-from humanoid import PoppyHumanoidEnv, clean_angles
+from humanoid import PoppyHumanoidEnv, convert_angles
 
 env = PoppyHumanoidEnv(pb.POSITION_CONTROL)
 N = len(env.joint_index)
@@ -19,14 +19,12 @@ angles[0].update(legs)
 
 waypoints = np.zeros((len(angles), N))
 for a,angle in enumerate(angles):
-    # cleaned = clean_angles(angle)
-    cleaned = {m: angle[m] * np.pi / 180 for m in angle}
+    cleaned = convert_angles(angle)
     for m,p in cleaned.items():
         waypoints[a, env.joint_index[m]] = p
 
 # initial angles/position
-for p, position in enumerate(waypoints[0]):
-    pb.resetJointState(env.robot_id, p, position)
+env.set_position(waypoints[0])
 pb.resetBasePositionAndOrientation(env.robot_id,
     (0, 0, .41),
     pb.getQuaternionFromEuler((0,0,0)))

pybullet/tasks/crawl/crawl.py
@@ -4,12 +4,7 @@ import sys
 sys.path.append('../../envs')
 
 import pybullet as pb
-from humanoid import PoppyHumanoidEnv, clean_angles
-
-# angles = []
-# for fn in ["preleft", "leftup", "leftswing", "leftstep"]:
-#     with open("../../../scripts/%s.pkl" % fn,"rb") as f:
-#         angles.append(pk.load(f))
+from humanoid import PoppyHumanoidEnv, convert_angles
 
 angles = [{}]
 
@@ -32,7 +27,7 @@ crawl_angles.update({
 # pybullet specific (not compliant):
 crawl_angles.update({
     # 'r_shoulder_x': -90, 'l_shoulder_x': -90, # somehow wrong in scripts?
-    'r_shoulder_y': 45, 'l_shoulder_y': 45,})
+    'r_shoulder_y': -45, 'l_shoulder_y': -45,}) # compliant in scripts
 
 angles = [crawl_angles]
 
@@ -41,7 +36,7 @@ angle_updates = [
     {'r_shoulder_x':12., 'abs_x': -3., 'abs_y': 37., 'abs_z': -15., 'r_elbow_y':-90.}, # rotate torso
     {'l_elbow_y':-90., 'abs_z': 0., 'l_shoulder_y': 0}, # left elbow perp
     {'l_shoulder_x':0., 'r_shoulder_x':0.},
-    {'r_elbow_y': -115., 'l_elbow_y': -115.,'l_hip_y': -70., 'l_knee_y': 100.,'r_hip_y': -70., 'r_knee_y': 100., 'l_shoulder_y': 45}, # forward thighs vertical
+    {'r_elbow_y': -115., 'l_elbow_y': -115.,'l_hip_y': -70., 'l_knee_y': 100.,'r_hip_y': -70., 'r_knee_y': 100., 'l_shoulder_y': -45}, # forward thighs vertical
     {'r_hip_z': 5., 'r_hip_y': -95., 'l_hip_y': -95., 'l_hip_z': 18., 'abs_z': 18., 'l_knee_y': 100., 'r_knee_y': 100.}, # hip raised
     {'r_hip_z': -2., 'r_hip_y': -100., 'l_hip_y': -70., 'l_hip_z': 2., 'abs_z': -15., 'l_knee_y': 100., 'r_knee_y': 125.}, # right leg forward
     {'r_hip_z': 0., 'r_hip_y': -100., 'l_hip_y': -100., 'l_hip_z': 0., 'abs_z': 0., 'l_knee_y': 125., 'r_knee_y': 125.}, # left leg forward
@@ -53,7 +48,7 @@ for changes in angle_updates:
     
 waypoints = np.zeros((len(angles), N))
 for a,angle in enumerate(angles):
-    cleaned = clean_angles(angle)
+    cleaned = convert_angles(angle)
     for m,p in cleaned.items():
         waypoints[a, env.joint_index[m]] = p
 
@@ -75,7 +70,9 @@ while True:
 
     target = waypoints[w]
     duration = .25
-    env.goto_position(target, duration)
+    env.goto_position(target, duration, hang=False)
+    # env.set_position(target)
+    # input('.')
     w = (w + 1) % len(waypoints)
 
 # input('.')