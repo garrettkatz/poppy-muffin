4fa846ad3f9c38d462ada710d125cac38d292e45
garrettkatz <garrett.katz@gmail.com>
2021-06-11 00:01:09 -0400

    data dumps in restack.py working but slow

--- pybullet/tasks/pick_and_place/restack.py
+++ pybullet/tasks/pick_and_place/restack.py
@@ -1,9 +1,24 @@
 import pybullet as pb
 
-class Restacker():
-    def __init__(self, env, goal_block_above):
+class DataDump:
+    def __init__(self):
+        self.data = []
+    def add_command(self, command):
+        self.data.append({
+            "command": command,
+            "records": []})
+    def step_hook(self, env, action):
+        if action is None or len(self.data) == 0: return
+        position = env.get_position()
+        delta = action - position
+        rgb, _, _, coords_of = env.get_camera_image()
+        self.data[-1]["records"].append((position, delta, rgb, coords_of))
+
+class Restacker:
+    def __init__(self, env, goal_block_above, dump=None):
         self.env = env
         self.goal_block_above = goal_block_above
+        self.dump = dump
 
     def free_spot(self):
         for base in self.env.bases:
@@ -14,15 +29,17 @@ class Restacker():
         for point, delta in waypoints: 
             targs = self.env.tip_targets_around(point, quat, delta)
             angles = self.env.inverse_kinematics([5, 7], targs)
-            self.env.goto_position(angles, 1)
+            self.env.goto_position(angles, .25)
     
     def pick_up(self, block):
+        if self.dump is not None: dump.add_command(("pick_up", (block,)))
         pos, quat = self.env.placement_of(block)
         stage = pos[:2] + (.1,)
         self.run_trajectory(quat, [(stage, .02), (pos, .02), (pos, .01), (stage, .01)])
         self.env.block_above[self.env.thing_below[block]] = 'none'
 
     def put_down_on(self, thing, block):
+        if self.dump is not None: dump.add_command(("put_down_on", (thing, block)))
         pos, quat = self.env.placement_of(thing)
         pos = pos[:2] + (pos[2] + .0201,)
         stage = pos[:2] + (.1,)    
@@ -62,19 +79,21 @@ if __name__ == "__main__":
 
     num_blocks = 7
     thing_below = {"b%d"%b: "t%d"%b for b in range(num_blocks)}
-    thing_below["b3"] = "b4"
-    thing_below["b4"] = "b5"
-    thing_below["b2"] = "b1"
+    # thing_below["b3"] = "b4"
+    # thing_below["b4"] = "b5"
+    # thing_below["b2"] = "b1"
 
     goal_thing_below = {"b%d"%b: "t%d"%b for b in range(num_blocks)}
     goal_thing_below["b3"] = "b2"
-    goal_thing_below["b2"] = "b6"
+    # goal_thing_below["b2"] = "b6"
+
+    dump = DataDump()
 
     import sys
     sys.path.append('../../envs')    
     from blocks_world import BlocksWorldEnv
     
-    env = BlocksWorldEnv(pb.POSITION_CONTROL)
+    env = BlocksWorldEnv(pb.POSITION_CONTROL, step_hook = dump.step_hook)
     env.load_blocks(thing_below)
 
     # from check/camera.py
@@ -83,6 +102,8 @@ if __name__ == "__main__":
         (-0.6051651835441589, 0.26229506731033325, -0.24448847770690918))
     
     goal_block_above = env.invert(goal_thing_below)
-    restacker = Restacker(env, goal_block_above)
+    restacker = Restacker(env, goal_block_above, dump)
     restacker.run()
+    
+    env.close()
 
