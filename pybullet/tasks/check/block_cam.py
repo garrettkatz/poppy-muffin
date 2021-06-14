a0ef0914f9384f2cf2e937d35b12a6d5fc1d2b7f
garrettkatz <garrett.katz@gmail.com>
2021-06-14 18:19:51 -0400

    checking block cam on outpost

---
+++ pybullet/tasks/check/block_cam.py
@@ -0,0 +1,19 @@
+import pickle as pk
+import numpy as np
+import sys
+sys.path.append('../../envs')
+
+import pybullet as pb
+from blocks_world import BlocksWorldEnv, random_thing_below
+
+thing_below = random_thing_below(num_blocks=7, max_levels=3)
+env = BlocksWorldEnv(pb.POSITION_CONTROL, show=False, control_period=12)
+env.load_blocks(thing_below)
+rgba, view, proj, coords_of = env.get_camera_image()
+env.close()
+
+import matplotlib.pyplot as pt
+pt.imshow(rgba)
+pt.show()
+
+
