0308be06a87e9eb80a9698b6e0160b43c0b483f9
garrettkatz <garrett.katz@gmail.com>
2021-06-19 01:40:26 -0400

    multiple RL repetitions ready to scale up

--- pybullet/tasks/pick_and_place/nvm_am_compare.py
+++ pybullet/tasks/pick_and_place/nvm_am_compare.py
@@ -91,22 +91,186 @@ if __name__ == "__main__":
                 # print(" size:")
                 # for sz in nvm_size: print(sz)
                 print(" nvm size: %d" % nvm_size[2])
-        
-        # scatter plots
-        pt.figure(figsize=(3,8))
-        for m, metric in enumerate(["Ticks", "Runtime (s)", "Symbolic reward", "Spatial reward"]):
-            pt.subplot(4,1,m+1)
-            for num_blocks in reversed(block_counts):
-                x = [am_result[m] for am_result, nvm_result, nvm_size in all_results[num_blocks].values()]
-                y = [nvm_result[m] for am_result, nvm_result, nvm_size in all_results[num_blocks].values()]
-                if m == 2:
-                    x = 0.1*np.random.rand(len(x)) + x
-                    y = 0.1*np.random.rand(len(y)) + y
-                pt.plot(x, y, '.', label="%d blocks" % num_blocks)
-            if m == 3: pt.xlabel("VM")
-            pt.ylabel("NVM")
-            if m == 0: pt.legend(loc="upper left")
-            pt.title(metric)
 
+        # reorganize by metric
+        # metrics[metric][num_blocks][am/nvm][rep]
+        metrics = {}
+        for m,met in enumerate(["ticks","time","sym","spa"]):
+            metrics[met] = {}
+            for num_blocks in block_counts:
+                metrics[met][num_blocks] = {}
+                for v,mach in enumerate(["rvm","nvm"]):
+                    metrics[met][num_blocks][mach] = []
+                    for rep in all_results[num_blocks].values():
+                        metrics[met][num_blocks][mach].append(rep[v][m])
+
+        # tick scatterplot
+        fig = pt.figure(figsize=(6,2.35))
+        gs = fig.add_gridspec(1,3)
+        # pt.subplot(1,2,1)
+        fig.add_subplot(gs[0,0])
+        for n,num_blocks in enumerate(metrics["ticks"].keys()):
+            x = metrics["ticks"][num_blocks]["rvm"]
+            y = metrics["ticks"][num_blocks]["nvm"]
+            # shade = [.4, .3, .2, .1, 0][n]
+            shade = 0
+            pt.scatter(x, y, ec=(shade,)*3, fc="none", marker="o")
+        pt.xticks([600, 900, 1200])
+        pt.yticks([600, 900, 1200])
+        pt.xlabel("RVM ticks", fontsize=12)
+        pt.ylabel("NVM ticks", fontsize=12)
+
+        # tick boxplot
+        fig.add_subplot(gs[0,1:])
+        # pt.subplot(1,2,2)
+        x = [metrics["ticks"][num_blocks]["nvm"] for num_blocks in metrics["ticks"]]
+        positions = list(sorted(metrics["ticks"].keys()))
+        pt.boxplot(x, positions=positions, medianprops={"c": "k"})
+        # pt.ylabel("Ticks")
+        pt.yticks([])
+        pt.xlabel("Blocks", fontsize=12)
+        
         pt.tight_layout()
+        pt.savefig("tick_compare.eps")
+        # pt.show()
+        pt.close()
+
+        # clock rates
+        for mach in ["rvm", "nvm"]:
+            runtimes = [rep for num_blocks in range(3,8)
+                for rep in metrics["time"][num_blocks][mach]]
+            numticks = [rep for num_blocks in range(3,8)
+                for rep in metrics["ticks"][num_blocks][mach]]
+            print("%s clock rate: %f Hz" % (mach, np.mean(np.array(numticks)/np.array(runtimes))))
+
+        # runtime scatterplot
+        fig = pt.figure(figsize=(6,2.35))
+        gs = fig.add_gridspec(1,5)
+        fig.add_subplot(gs[0,:2])
+        for n,num_blocks in enumerate(reversed(sorted(metrics["time"].keys()))):
+            x = metrics["time"][num_blocks]["rvm"]
+            y = metrics["time"][num_blocks]["nvm"]
+            shade = np.linspace(0, .7, 5)[n]
+            # shade = 0
+            pt.scatter(x, y, fc=(shade,)*3, ec="none", marker="o", label=str(num_blocks))
+        # pt.xticks([600, 900, 1200])
+        # pt.yticks([600, 900, 1200])
+        pt.legend()
+        pt.xlabel("RVM runtime (s)", fontsize=12)
+        pt.ylabel("NVM runtime (s)", fontsize=12)
+
+        # runtime histogram
+        fig.add_subplot(gs[0,2:])
+        for m,mach in enumerate(["rvm","nvm"]):
+            x = [rep for num_blocks in metrics["time"]
+                    for rep in metrics["time"][num_blocks][mach]]
+            pt.hist(x, ec="k", fc=["w",(.5,)*3][m], bins=np.linspace(0,10,20), label=mach.upper())
+        pt.ylabel("Frequency", fontsize=12)
+        pt.xlabel("Runtime (s)", fontsize=12)
+        pt.legend()
+        
+        pt.tight_layout()
+        pt.savefig("time_compare.eps")
+        # pt.show()
+        pt.close()
+
+        # sym box plot
+        fig = pt.figure(figsize=(6,2.35))
+        block_counts = list(sorted(metrics["sym"].keys()))
+        positions = np.arange(len(block_counts))
+        handles = [0,0]
+        for m,mach in enumerate(["rvm", "nvm"]):
+            x = [-np.array(metrics["sym"][num_blocks][mach]) for num_blocks in block_counts]
+            parts = pt.violinplot(x, positions=2*positions + m, widths=.9)
+            shade = [.75, .25][m]
+            for pc in parts["bodies"]:
+                pc.set_facecolor((shade,)*3)
+                handles[m] = pc
+                pc.set_edgecolor('k')
+                pc.set_alpha(1)
+            for key in ["cmins","cmaxes","cbars"]:
+                parts[key].set_edgecolor('k')
+                parts[key].set_alpha(1)
+        pt.ylabel("Symbolic distance", fontsize=12)
+        # pt.yticks([])
+        pt.xticks(.5 + 2*positions, block_counts)
+        pt.xlabel("Number of blocks in problem instance", fontsize=12)
+        pt.yticks(range(len(block_counts)+1))
+        pt.legend(handles, ["RVM", "NVM"], loc='upper left')
+        
+        pt.tight_layout()
+        # pt.savefig("sym_compare.eps")
+        pt.savefig("sym_compare.png")
+        # pt.show()
+        pt.close()
+
+        # # spa box plot
+        # fig = pt.figure(figsize=(6,2.35))
+        # block_counts = list(sorted(metrics["spa"].keys()))
+        # positions = np.arange(len(block_counts))
+        # handles = [0,0]
+        # for m,mach in enumerate(["rvm", "nvm"]):
+        #     x = [-np.array(metrics["spa"][num_blocks][mach]) for num_blocks in block_counts]
+        #     parts = pt.violinplot(x, positions=2*positions + m, widths=.9)
+        #     shade = [.75, .25][m]
+        #     for pc in parts["bodies"]:
+        #         pc.set_facecolor((shade,)*3)
+        #         handles[m] = pc
+        #         pc.set_edgecolor('k')
+        #         pc.set_alpha(1)
+        #     for key in ["cmins","cmaxes","cbars"]:
+        #         parts[key].set_edgecolor('k')
+        #         parts[key].set_alpha(1)
+        # pt.ylabel("Spatial distance", fontsize=12)
+        # # pt.yticks([])
+        # pt.xticks(.5 + 2*positions, block_counts)
+        # pt.xlabel("Number of blocks in problem instance", fontsize=12)
+        # pt.yticks(range(len(block_counts)+1))
+        # pt.legend(handles, ["RVM", "NVM"], loc='upper left')
+        
+        # pt.tight_layout()
+        # # pt.savefig("spa_compare.eps")
+        # pt.savefig("spa_compare.png")
+        # # pt.show()
+        # pt.close()
+
+        # spa scatterplot
+        fig = pt.figure(figsize=(5,3.25))
+        for n,num_blocks in enumerate(reversed(sorted(metrics["spa"].keys()))):
+            x = -np.array(metrics["spa"][num_blocks]["rvm"])
+            y = -np.array(metrics["spa"][num_blocks]["nvm"])
+            shade = np.linspace(0, .7, 5)[n]
+            # shade = 0
+            pt.scatter(x, y, fc=(shade,)*3, ec="none", marker="o", label=str(num_blocks))
+        pt.xlim([-.2, 8.5])
+        pt.ylim([-.2, 6.5])
+        pt.xticks(np.arange(7))
+        pt.yticks(np.arange(7))
+        pt.legend(loc='lower right')
+        pt.xlabel("RVM spatial distance", fontsize=12)
+        pt.ylabel("NVM spatial distance", fontsize=12)
+        # pt.axis("equal")
+        pt.tight_layout()
+        pt.savefig("spa_compare.eps")
+        pt.savefig("spa_compare.png")
         pt.show()
+        pt.close()
+        
+        # # scatter plots
+        # pt.figure(figsize=(3,8))
+        # for m, metric in enumerate(["Ticks", "Runtime (s)", "Symbolic reward", "Spatial reward"]):
+        #     pt.subplot(4,1,m+1)
+        #     for num_blocks in reversed(block_counts):
+        #         x = [am_result[m] for am_result, nvm_result, nvm_size in all_results[num_blocks].values()]
+        #         y = [nvm_result[m] for am_result, nvm_result, nvm_size in all_results[num_blocks].values()]
+        #         if m == 2:
+        #             x = 0.1*np.random.rand(len(x)) + x
+        #             y = 0.1*np.random.rand(len(y)) + y
+        #         pt.plot(x, y, '.', label="%d blocks" % num_blocks)
+        #     if m == 3: pt.xlabel("VM")
+        #     pt.ylabel("NVM")
+        #     if m == 0: pt.legend(loc="upper left")
+        #     pt.title(metric)
+
+        # pt.tight_layout()
+        # pt.show()
--- pybullet/tasks/pick_and_place/reinforce_joints.py
+++ pybullet/tasks/pick_and_place/reinforce_joints.py
@@ -8,147 +8,168 @@ from restack import random_problem_instance, compute_spatial_reward, compute_sym
 from abstract_machine import make_abstract_machine, memorize_env
 from nvm import virtualize
 
+# ~ 11s per episode
+# 12 hours = 8*60*60s = 11s * 30epis * epochs * 3reps
+# epochs = 12*60*60 / (11*30*3) = 
+
 if __name__ == "__main__":
 
     num_blocks, max_levels, num_bases = 6, 3, 6
-    num_episodes = 30
-    num_epochs = 100
+    num_repetitions = 2
+    num_episodes = 2
+    num_epochs = 2
     
     run_exp = False
-    if run_exp:
-        results = []
-
-        env = BlocksWorldEnv(show=False)
-        env.load_blocks({"b%d" % n: "t%d" % n for n in range(num_bases)}) # placehold for nvm init
-    
-        # tr.autograd.set_detect_anomaly(True)
-    
-        am = make_abstract_machine(env, num_bases, max_levels)
-        nvm = virtualize(am)
-        # print(nvm.size())
-        # input('.')
-    
-        # set up trainable connections
-        conn_params = {
-            name: nvm.connections[name].W
-            # for name in ["ik", "to", "tc", "pc", "pc"]
-            for name in ["ik"]
-        }
-        for p in conn_params.values(): p.requires_grad_()
-        opt = tr.optim.Adam(conn_params.values(), lr=0.00001)
-        
-        orig_ik = nvm.connections["ik"].W.clone()
-    
-        # env.reset()
-        # thing_below, goal_thing_below = random_problem_instance(env, num_blocks, max_levels, num_bases)
-
-        # failure case:
-        thing_below = {'b0': 't1', 'b2': 'b0', 'b4': 'b2', 'b1': 't4', 'b3': 't2'}
-        goal_thing_below = {'b1': 't1', 'b2': 't3', 'b3': 'b2', 'b0': 't0', 'b4': 'b0'}
-
-        # thing_below = {"b%d"%b: "t%d"%b for b in range(num_blocks)}
-        # thing_below.update({"b1": "b0", "b2": "b3"})    
-        # goal_thing_below = {"b%d"%b: "t%d"%b for b in range(num_blocks)}
-        # goal_thing_below.update({"b1": "b2", "b2": "b0"})
-    
-        goal_thing_above = nvm.env.invert(goal_thing_below)
-        for key, val in goal_thing_above.items():
-            if val == "none": goal_thing_above[key] = "nil"
-    
-        lc = []
-        for epoch in range(num_epochs):
-            epoch_rewards = []
-            for episode in range(num_episodes):
-        
-                env.reset()
-                env.load_blocks(thing_below, num_bases)
-        
-                # reset trainable parameters
-                for name, param in conn_params.items():
-                    nvm.connections[name].W = param
-                
-                # input env
-                memorize_env(nvm, goal_thing_above)
-            
-                reset_dict = {"jnt": tr.tensor(am.ik["rest"]).float()}
-                # reset_dict.update({"obj": nvm.registers["obj"].encode("t0")}) # stack all
-                # reset_dict.update({"r0": nvm.registers["r0"].encode("b0")}) # stack on
-                nvm.reset(reset_dict)
-            
-                sigma = 0.001 # radians
-                log_prob = 0.0 # accumulate over episode
-            
-                dbg = False
-                nvm.mount("main")
-                # nvm.mount("stack_all")
-                # nvm.mount("stack_on")
-                if dbg: nvm.dbg()
-                target_changed = True
-                while True:
-                    done = nvm.tick()
-                    if dbg: nvm.dbg()
-                    if target_changed:
-                        mu = nvm.registers["jnt"].content
-                        dist = tr.distributions.normal.Normal(mu, sigma)
-                        if episode == 0:
-                            position = mu.detach().clone()
-                        else:
-                            position = dist.sample()
-                            log_probs = dist.log_prob(position)
-                            log_prob += log_probs.sum() # multivariate white noise
-                        nvm.env.goto_position(position.detach().numpy())
-                    tar = nvm.registers["tar"]
-                    target_changed = (tar.decode(tar.content) != tar.decode(tar.old_content))
-                    if done: break
-                
-                spa_reward = compute_spatial_reward(nvm.env, goal_thing_below)
-                sym_reward = compute_symbolic_reward(nvm.env, goal_thing_below)
-                reward = sym_reward + 0.01*spa_reward
-                epoch_rewards.append(reward)
-                
-                if episode != 0:
-                    loss = -reward * log_prob
-                    loss.backward()
-
-                print("  %d(%d): r = %f, lp= %f" % (epoch, episode, reward, log_prob))
-
-            # update params based on episodes
-            opt.step()
-            opt.zero_grad()
-            
-            delta = (orig_ik - conn_params["ik"]).abs().max()
-            avg_reward = np.mean(epoch_rewards)
-            std_reward = np.std(epoch_rewards)
-            
-            results.append((epoch_rewards, delta.item()))
-            print("%d: R = %f (+/- %f), dW = %f" % (epoch, avg_reward, std_reward, delta))
-            with open("rj.pkl","wb") as f: pk.dump(results, f)
-    
     showresults = True
-    if showresults:
-        with open("rj.pkl","rb") as f: results = pk.load(f)
-        epoch_rewards, deltas = zip(*results)
-        num_epochs = len(results)
-    
-        print("LC:")
-        for r,rewards in enumerate(epoch_rewards): print(r, rewards)
-        
-        import matplotlib.pyplot as pt
-        pt.plot([np.mean(rewards) for rewards in epoch_rewards], 'k-')
-        pt.plot([rewards[0] for rewards in epoch_rewards], 'b-')
-        x, y = zip(*[(r,reward) for r in range(num_epochs) for reward in epoch_rewards[r]])    
-        pt.plot(x, y, 'k.')
 
-        # pt.plot(np.log(-np.array(rewards)))
-        # pt.ylabel("log(-R)")
+    if run_exp:
+    
+        results = []
+        for rep in range(num_repetitions):
+            start_rep = time.perf_counter()
+            results.append([])
+
+            env = BlocksWorldEnv(show=False)
+            env.load_blocks({"b%d" % n: "t%d" % n for n in range(num_bases)}) # placehold for nvm init
         
-        # trend line
-        avg_rewards = np.mean(epoch_rewards, axis=1)
-        window = 5
-        for r in range(len(avg_rewards)-window):
-            avg_rewards[r] += avg_rewards[r+1:r+window].sum()
-            avg_rewards[r] /= window
-        pt.plot(avg_rewards, 'ro-')
+            # tr.autograd.set_detect_anomaly(True)
+        
+            am = make_abstract_machine(env, num_bases, max_levels)
+            nvm = virtualize(am)
+            # print(nvm.size())
+            # input('.')
+        
+            # set up trainable connections
+            conn_params = {
+                name: nvm.connections[name].W
+                # for name in ["ik", "to", "tc", "pc", "pc"]
+                for name in ["ik"]
+            }
+            for p in conn_params.values(): p.requires_grad_()
+            opt = tr.optim.Adam(conn_params.values(), lr=0.00001)
+            
+            orig_ik = nvm.connections["ik"].W.clone()
+        
+            # # one failure case in all episodes:
+            # thing_below = {'b0': 't1', 'b2': 'b0', 'b4': 'b2', 'b1': 't4', 'b3': 't2'}
+            # goal_thing_below = {'b1': 't1', 'b2': 't3', 'b3': 'b2', 'b0': 't0', 'b4': 'b0'}
+            # goal_thing_above = nvm.env.invert(goal_thing_below)
+            # for key, val in goal_thing_above.items():
+            #     if val == "none": goal_thing_above[key] = "nil"
+        
+            lc = []
+            for epoch in range(num_epochs):
+                start_epoch = time.perf_counter()
+                epoch_rewards = []
+
+                for episode in range(num_episodes):
+                    start_episode = time.perf_counter()
+            
+                    # different problem instance each episode
+                    env.reset()
+                    thing_below, goal_thing_below = random_problem_instance(
+                        env, num_blocks, max_levels, num_bases)
+                    goal_thing_above = env.invert(goal_thing_below)
+                    for key, val in goal_thing_above.items():
+                        if val == "none": goal_thing_above[key] = "nil"
+            
+                    # reset trainable parameters
+                    for name, param in conn_params.items():
+                        nvm.connections[name].W = param
+                    
+                    # input env
+                    memorize_env(nvm, goal_thing_above)
+                
+                    reset_dict = {"jnt": tr.tensor(am.ik["rest"]).float()}
+                    nvm.reset(reset_dict)
+                
+                    sigma = 0.001 # radians
+                    log_prob = 0.0 # accumulate over episode
+                
+                    dbg = False
+                    nvm.mount("main")
+                    if dbg: nvm.dbg()
+                    target_changed = True
+                    while True:
+                        done = nvm.tick()
+                        if dbg: nvm.dbg()
+                        if target_changed:
+                            mu = nvm.registers["jnt"].content
+                            dist = tr.distributions.normal.Normal(mu, sigma)
+                            if episode == 0:
+                                position = mu.detach().clone()
+                            else:
+                                position = dist.sample()
+                                log_probs = dist.log_prob(position)
+                                log_prob += log_probs.sum() # multivariate white noise
+                            nvm.env.goto_position(position.detach().numpy())
+                        tar = nvm.registers["tar"]
+                        target_changed = (tar.decode(tar.content) != tar.decode(tar.old_content))
+                        if done: break
+                    
+                    spa_reward = compute_spatial_reward(nvm.env, goal_thing_below)
+                    # sym_reward = compute_symbolic_reward(nvm.env, goal_thing_below)
+                    # reward = sym_reward + 0.5*spa_reward
+                    reward = spa_reward
+                    epoch_rewards.append(reward)
+                    
+                    if episode != 0:
+                        loss = -reward * log_prob
+                        loss.backward()
+    
+                    episode_time = time.perf_counter() - start_episode
+                    print("    %d,%d,%d: r = %f, lp= %f, took %fs" % (
+                        rep, epoch, episode, reward, log_prob, episode_time))
+    
+                # update params based on episodes
+                opt.step()
+                opt.zero_grad()
+                
+                delta = (orig_ik - conn_params["ik"]).abs().max()
+                avg_reward = np.mean(epoch_rewards)
+                std_reward = np.std(epoch_rewards)
+                
+                results[-1].append((epoch_rewards, delta.item()))
+                with open("rj.pkl","wb") as f: pk.dump(results, f)
+                epoch_time = time.perf_counter() - start_epoch
+                print(" %d,%d: R = %f (+/- %f), dW = %f" % (rep, epoch, avg_reward, std_reward, delta))
+                print(" %d,%d took %fs" % (rep, epoch, epoch_time))
+
+            env.close()
+            rep_time = time.perf_counter() - start_rep
+            print("%d took %fs" % (rep, rep_time))
+    
+    if showresults:
+        import matplotlib.pyplot as pt
+
+        with open("rj.pkl","rb") as f: results = pk.load(f)
+        # with open("rj_1e-5_spa.pkl","rb") as f: results = [pk.load(f)]
+        # with open("rj_1e-5_sym_spa_.01.pkl","rb") as f: results = [pk.load(f)]
+        
+        num_repetitions = len(results)
+        for rep in range(num_repetitions):
+            epoch_rewards, deltas = zip(*results[rep])
+            num_epochs = len(results[rep])
+        
+            print("rep %d LC:" % rep)
+            for r,rewards in enumerate(epoch_rewards): print(r, rewards)
+            
+            pt.subplot(num_repetitions, 1, rep+1)
+            pt.plot([np.mean(rewards) for rewards in epoch_rewards], 'k-')
+            pt.plot([rewards[0] for rewards in epoch_rewards], 'b-')
+            x, y = zip(*[(r,reward) for r in range(num_epochs) for reward in epoch_rewards[r]])    
+            pt.plot(x, y, 'k.')
+    
+            # pt.plot(np.log(-np.array(rewards)))
+            # pt.ylabel("log(-R)")
+            
+            # trend line
+            avg_rewards = np.mean(epoch_rewards, axis=1)
+            window = 5
+            for r in range(len(avg_rewards)-window):
+                avg_rewards[r] += avg_rewards[r+1:r+window].sum()
+                avg_rewards[r] /= window
+            pt.plot(avg_rewards, 'ro-')
 
         pt.show()
     
