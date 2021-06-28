1078800e0da40bfa98d1c741692499333a5fe02d
garrettkatz <garrett.katz@gmail.com>
2021-06-28 19:51:52 -0400

    nvm not crashing with batch FSER

--- pybullet/tasks/pick_and_place/neural_virtual_machine.py
+++ pybullet/tasks/pick_and_place/neural_virtual_machine.py
@@ -5,9 +5,9 @@ def default_activator(v):
     return tr.tanh(v) / tanh1
 
 def fast_store_erase_rule(W, x, aσy):
-    x, aσy = x.reshape(-1,1), aσy.reshape(-1,1)
-    dW = (aσy - W.mm(x)) * x.t() / float(W.shape[1])
-    return dW
+    # W: (batch, x size, y size)
+    # x, aσy: (batch, size, 1)
+    return (aσy - W @ x) * x.transpose(1, 2) / float(x.shape[1])
 
 class NeuralVirtualMachine:
     def __init__(self,
@@ -33,16 +33,14 @@ class NeuralVirtualMachine:
             self.connections_to[r] += ((c, q),)
         
         # set up gate layer indexing
-        self.recall_index = {}
         self.recall_slice_for = {}
+        recall_offset = 0
         for r in self.register_names:
             self.recall_slice_for[r] = slice(
-                len(self.recall_index),
-                len(self.recall_index) + len(self.connections_to[r]))
-            for c, q in self.connections_to[r]:
-                self.recall_index[c] = len(self.recall_index)
-        self.storage_index = {
-            c: len(self.recall_index) + i for i,c in enumerate(plastic_connections)}
+                recall_offset,
+                recall_offset + len(self.connections_to[r]))
+            recall_offset += len(self.connections_to[r])
+        self.storage_index = {c: recall_offset + i for i,c in enumerate(plastic_connections)}
         
         # set up state sequence buffers
         self.tick_counter = 0
@@ -53,12 +51,21 @@ class NeuralVirtualMachine:
             c: {0: tr.zeros((1,self.register_sizes[r], self.register_sizes[q]))}
             for c, (q, r) in self.connectivity.items()}
     
-    def unpack(self, g):
-        u = {c: g[:, self.recall_index[c]] for c in self.connectivity} #
-        ρ = {r: g[:, self.recall_slice_for[r]] for r in self.register_names}
-        ℓ = {c: g[:, self.storage_index[c]] for c in self.plastic_connections}
-        return u, ρ, ℓ
+    def batchify_weights(self, W):
+        if len(W.shape) == 3: return W
+        if len(W.shape) == 2: return W.unsqueeze(dim=0)
+        raise ValueError("W should be 2 or 3 dimensional, not %d dimensional" % len(W.shape))
 
+    def batchify_activities(self, v):
+        if len(v.shape) == 3: return v
+        if len(v.shape) == 1: return v.reshape(1, len(v), 1)
+        raise ValueError("v should be 1 or 3 dimensional, not %d dimensional" % len(v.shape))
+    
+    def unpack(self, g):
+        u = {r: g[:, self.recall_slice_for[r]] for r in self.register_names}
+        ℓ = {c: g[:, [self.storage_index[c]]] for c in self.plastic_connections}
+        return u, ℓ
+    
     def tick(self, W_in={}, v_in={}):
         # setup variables
         t = self.tick_counter
@@ -69,32 +76,25 @@ class NeuralVirtualMachine:
         
         # set inputs
         for c in W_in:
-            if t in W_in[c]: W[c][t] = W_in[c][t]
+            if t in W_in[c]: W[c][t] = self.batchify_weights(W_in[c][t])
         for r in v_in:
-            if t in v_in[r]: v[r][t] = v_in[r][t]
+            if t in v_in[r]: v[r][t] = self.batchify_activities(v_in[r][t])
 
         # unpack gate values
-        u, ρ, ℓ = self.unpack(v[self.gate_register_name][t])
+        u, ℓ = self.unpack(v[self.gate_register_name][t])
 
         # recall
         for r in self.register_names:
-            v[r][t+1] = tr.zeros_like(v[r][t])
-            for c, q in self.connections_to[r]:
-                v[r][t+1] += u[c] * (W[c][t] @ v[q][t])
-            v[r][t+1] = σ[r](v[r][t+1])
-        for r in self.register_names:
-            if len(self.connections_to[r]) == 0:
-                v[r][t+1] = tr.zeros_like(v[r][t])
-            else:
+            v[r][t+1] = v[r][t]
+            if len(self.connections_to[r]) > 0:
                 Wv = [W[c][t] @ v[q][t] for c, q in self.connections_to[r]]
-                v[r][t+1] = σ[r](tr.cat(Wv, dim=-1) @ ρ[r])
+                v[r][t+1] = σ[r](tr.cat(Wv, dim=2) @ u[r])
 
         # storage
         for c, (q, r) in self.connectivity.items():
+            W[c][t+1] = W[c][t]
             if c in self.plastic_connections:
                 W[c][t+1] = W[c][t] + ℓ[c] * H(W[c][t], v[q][t], v[r][t])
-            else:
-                W[c][t+1] = W[c][t]
         
         # update counter
         self.tick_counter += 1
@@ -114,12 +114,15 @@ if __name__ == "__main__":
         "gts": ("ipt", "gts")}
     plastic_connections = [] #["r0>r1"]
 
-    W0 = tr.ones((1,register_sizes["r1"], register_sizes["r0"]), requires_grad=True)
-    v0 = tr.ones((1,register_sizes["r0"],1))
+    # W0 = tr.ones((1,register_sizes["r1"], register_sizes["r0"]), requires_grad=True)
+    # v0 = tr.ones((1,register_sizes["r0"],1))
+    W0 = tr.ones((register_sizes["r1"], register_sizes["r0"]), requires_grad=True)
+    v0 = tr.ones((register_sizes["r0"],))
 
     nvm = NeuralVirtualMachine(
         register_sizes, activators, gate_register_name,
         connectivity, plastic_connections)
+    
     W, v = nvm.run(v_in = {"r0": {0: v0, 1: v0}}, W_in = {"r0>r1": {0: W0}}, num_time_steps=2)
     print(nvm.tick_counter)
     print(W)
