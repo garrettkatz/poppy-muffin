c5ab2d017120264bfc98e34746d055ff60b115af
garrettkatz <garrett.katz@gmail.com>
2022-10-28 17:42:47 -0400

    tested gobuf with safe temp/voltage limits

--- scripts/launch.py
+++ scripts/launch.py
@@ -4,7 +4,6 @@ import pypot.utils.pypot_time as time
 import pickle as pk
 import numpy as np
 import matplotlib.pyplot as plt
-import dances
 from record_angles import hotkeys
 
 p = PH()
@@ -14,27 +13,55 @@ def gobuf(angs, duration, bufsize):
 
     # buffers
     bufkeys = ("position", "speed", "load", "voltage", "temperature")
-    buffers = {key: np.empty((bufsize, len(angs)) for key in bufkeys}
+    buffers = {key: np.empty((bufsize, len(angs))) for key in bufkeys}
     timepoints = np.linspace(0, duration, bufsize)
     motor_names = sorted(angs.keys())
+    success = True
+    t = 0
 
-    start = time.time()
-    p.goto_position(angs, duration, wait=False)
+    try:
 
-    for t in range(bufsize):
+        start = time.time()
+        p.goto_position(angs, duration, wait=False)
 
-        # update buffer
-        for key in bufkeys:
-            buffers['position'][t] = [
-                getattr(getattr(p, motor_name), "present_" + key)
-                for motor_name in motor_names]
+        for t in range(bufsize):
 
-        # sleep to next timepoint
-        if t < bufsize - 1:
-            time_elapsed = time.time() - start
-            time.sleep(max(0, timepoints[t+1] - time_elapsed)
+            # update buffer
+            for key in bufkeys:
+                buffers[key][t] = [
+                    getattr(getattr(p, motor_name), "present_" + key)
+                    for motor_name in motor_names]
 
-    return buffers, motor_names
+            # abort if approaching dangerous voltage/temperature
+            # https://emanual.robotis.com/docs/en/dxl/mx/mx-12w/#control-table-of-eeprom-area
+            # https://emanual.robotis.com/docs/en/dxl/mx/mx-28/#control-table-of-eeprom-area
+            # https://emanual.robotis.com/docs/en/dxl/mx/mx-64/#control-table-of-eeprom-area
+            # pypot/dynamixel/conversion.py converts dynamixel voltage register to volts (divides by 10)
+            for m in p.motors:
+                safe_voltage = (7 < m.present_voltage < 15)
+                safe_temperature = (m.present_temperature < 70)
+                if not (safe_voltage and safe_temperature):
+                    success = False
+            if not success:
+                for m in p.motors:
+                    m.goal_position = m.present_position
+                for key in bufkeys:
+                    buffers[key] = buffers[key][:t+1]
+                print("Unsafe motion!!!")
+                break
+
+            # sleep to next timepoint
+            if t < bufsize - 1:
+                time_elapsed = time.time() - start
+                print(t, time_elapsed, timepoints[t])
+                time.sleep(max(0, timepoints[t+1] - time_elapsed))
+
+        return success, buffers, motor_names
+
+    except:
+
+        buffers = {key: buf[:t] for (key, buf) in buffers.items()}
+        with open("gobuf.pkl","wb") as f: pk.dump((success, buffers, motor_names), f)
 
 def angs():
     return {m.name: m.present_position for m in p.motors}
