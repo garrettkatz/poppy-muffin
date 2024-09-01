0caceb65b50ae341fabce68527ab57a436213acd
garrettkatz <garrett.katz@gmail.com>
2024-09-01 11:17:10 -0400

    initial rrt without pareto considerations

--- pybullet/tasks/check/quat_distance.py
+++ pybullet/tasks/check/quat_distance.py
@@ -1,12 +1,19 @@
 import numpy as np
 import pybullet as pb
 
-q1 = pb.getQuaternionFromEuler((0, 0, 0))
-q2 = pb.getQuaternionFromEuler((0.001, 0.001, 0.001))
+q1 = pb.getQuaternionFromEuler((1, 1, 1))
+# q2 = pb.getQuaternionFromEuler((0.001, 0.001, 0.001))
+q2 = pb.getQuaternionFromEuler((-1, -2, -3))
 
 print(q1)
 print(q2)
 
 d = pb.getDifferenceQuaternion(q1, q2)
+print(d)
+
+axis, ang = pb.getAxisAngleFromQuaternion(d)
+print(axis, ang)
+
 theta = 2*np.arccos(abs(sum([v1*v2 for (v1, v2) in zip(q1, q2)])))
 print(theta)
+
--- pybullet/tasks/walk/conncomp.py
+++ pybullet/tasks/walk/conncomp.py
@@ -31,7 +31,13 @@ def traj_from_array(arr, num_cycles):
     init_dict = env.mirror_dict(step_traj[-1][1])
     return init_dict, cycle_traj
 
-def run_traj(env, init_dict, traj):
+def run_traj(env, step_array, num_cycles):
+    # step_array[t]: angle array at timestep t (1-based) for a single footstep.
+    # it is mirrored for the second foot of the cycle
+    # the cycle is repeated the requested number of times
+    # step_array[-1] is mirrored for the initial pose before the trajectory
+
+    init_dict, cycle_traj = traj_from_array(step_array, num_cycles)
 
     # start fresh
     env.reset()
@@ -58,49 +64,180 @@ def run_traj(env, init_dict, traj):
 
     # try performing trajectory with another settle at end
     positions = env.track_trajectory(cycle_traj + [(2, init_dict)])
+
+    # get state after settle
     post_base, post_quat = pb.getBasePositionAndOrientation(env.robot_id)
+    post_linv, post_angv = pb.getBaseVelocity(env.robot_id)
 
     # return results
-    return positions, init_base, init_quat, post_base, post_quat
+    return positions, init_base, init_quat, post_base, post_quat, post_linv, post_angv
 
-def compute_jerk(positions, timestep):
+def is_fall(init_base, post_base, relative_tolerance = .05):
+    return (np.fabs(init_base[2] - post_base[2]) / init_base[2]) > relative_tolerance
+
+# all objectives are minimizations
+
+def compute_jerk(positions):
     # minimum path length in joint space (or numerical velocity/jerk)
-    dp = (positions[1:] - positions[:-1]) / env.timestep
-    dv = (dp[1:] - dp[:-1]) / env.timestep
-    da = (dv[1:] - dv[:-1]) / env.timestep
-    return (da**2).sum() * timestep
+    # modulo constant scale factor (mean instead of sum, timestep not included)
+    dp = (positions[1:] - positions[:-1])
+    dv = (dp[1:] - dp[:-1])
+    da = (dv[1:] - dv[:-1])
+    return (da**2).mean()
 
+def forward_motion(init_base, post_base):
+    # returns change in base y-coordinate
+    # "forward" direction is negative y, lower is better
+    return post_base[1] - init_base[1]
+
+def lateral_motion(init_base, post_base):
+    return np.fabs(init_base[0] - post_base[0])
+
+def base_spin(init_quat, post_quat):
+    diff_quat = pb.getDifferenceQuaternion(init_quat, post_quat)
+    axis, ang = pb.getAxisAngleFromQuaternion(diff_quat)
+    return ang
+
+def ending_velocity(post_linv, post_angv):
+    return np.sum(np.array(post_linv + post_angv)**2)
+
+def get_objectives(result, relative_tolerance = .05):
+    # returns is_fall, other objectives
+    positions, init_base, init_quat, post_base, post_quat, post_linv, post_angv = result
+    return is_fall(init_base, post_base, relative_tolerance), (
+        forward_motion(init_base, post_base),
+        lateral_motion(init_base, post_base),
+        base_spin(init_quat, post_quat),
+        ending_velocity(post_linv, post_angv),
+        compute_jerk(positions),
+    )
 
 if __name__ == "__main__":
 
+    # setup paramters
     num_timesteps = int(np.ceil(duration / timestep))
     duration = num_timesteps * timestep
-    # input(f"{num_timesteps} timesteps...")
+    num_cycles = 1
+
+    relative_fall_tolerance = 0.05
+
+    do_rrt = False
+    show_rrt = False
+    show_best = True
+    max_samples = 50
     
-    env = PoppyHumanoidEnv(pb.POSITION_CONTROL, show=True)
+    # ### test one random traj
+
+    # env = PoppyHumanoidEnv(pb.POSITION_CONTROL, show=True)
+
+    # rand_array = np.random.randint(-1, 2, (num_timesteps, env.num_joints))    
+    # result = run_traj(env, rand_array, num_cycles = 1)
+    # positions, init_base, init_quat, post_base, post_quat, post_linv, post_angv = result
     
-    zero_array = np.zeros((num_timesteps, env.num_joints))
-    # init_dict, cycle_traj = traj_from_array(zero_array, num_cycles = 1)
+    # print('init base vs post base')
+    # print(init_base, post_base)
+    # print('init quat vs post quat')
+    # print(init_quat, post_quat)
+    # print(f'jerk = {compute_jerk(positions)}')
+    # print(f'fall = {is_fall(init_base, post_base)}')
+    # print(f'forward = {forward_motion(init_base, post_base)}')
+    # print(f'lateral = {lateral_motion(init_base, post_base)}')
+    # print(f'spin = {base_spin(init_quat, post_quat)}')
+    # print(f'vel = {ending_velocity(post_linv, post_angv)}')
+
+    # input('.')
+    # env.close()
     
-    rand_array = np.random.randint(-1, 2, (num_timesteps, env.num_joints))
-    init_dict, cycle_traj = traj_from_array(rand_array, num_cycles = 1)
+    # # import matplotlib.pyplot as pt
+    # # pt.plot(positions * 180. / np.pi)
+    # # pt.show()
+
+    ### RRT
+
+    if do_rrt:
+
+        env = PoppyHumanoidEnv(pb.POSITION_CONTROL, show=show_rrt)
     
-    result = run_traj(env, init_dict, cycle_traj)
-    positions, init_base, init_quat, post_base, post_quat = result
+        # allocate samples
+        all_samples = np.empty((max_samples, num_timesteps, env.num_joints))
+        all_falls = np.empty(max_samples, dtype=bool)
+        all_objectives = np.empty((max_samples, 5))
     
-    # print("zero cycle")
-    # for (dur, angs) in zero_cycle_traj: print(dur, angs)
-    # input("..")
+        # root tree at zero
+        all_samples[0,:,:] = 0
+        num_samples = 1
+        result = run_traj(env, all_samples[0], num_cycles)
+        all_falls[0], all_objectives[0] = get_objectives(result, relative_fall_tolerance)
     
-    env.close()
+        while num_samples < max_samples:
+            print(f"{num_samples} of {max_samples} samples...")
     
-    print('init base vs post base')
-    print(init_base, post_base)
-    print('init quat vs post quat')
-    print(init_quat, post_quat)
-    print(f'jerk = {compute_jerk(positions, timestep)}')
+            # sample random configuration
+            rand_array = np.random.uniform(-360, 360, (num_timesteps, env.num_joints))
+        
+            # find closest configuration so far
+            dists = np.sum((all_samples[:num_samples] - rand_array)**2, axis=(1,2))
+            closest = np.argmin(dists)
+            print(f"closest = {closest}")
+
+            # if closest was a fall, reject sample
+            if all_falls[closest]:
+                print("[fall]")
+                continue
+            
+            # normalize update direction to the double-unit cube and round to nearest degree
+            direction = rand_array - all_samples[closest]
+            direction = direction / np.fabs(direction).max()
+            direction = direction.round()
+        
+            # evaluate new sample
+            all_samples[num_samples] = all_samples[closest] + direction
+            result = run_traj(env, all_samples[num_samples], num_cycles)
+            all_falls[num_samples], all_objectives[num_samples] = get_objectives(result, relative_fall_tolerance)
+            num_samples += 1
+        
+            print('objs: progress, lateral, spin, velocity, jerk')
+            print(f'fall = {all_falls[num_samples-1]}, objs = {all_objectives[num_samples-1]}')
+        
+            # input('.')
     
-    import matplotlib.pyplot as pt
-    pt.plot(positions * 180. / np.pi)
-    pt.show()
+        np.savez("rrt.npz", samples=all_samples, falls=all_falls, objectives=all_objectives)
     
+        env.close()
+
+    npz = np.load("rrt.npz")
+    all_samples = npz["samples"]
+    all_falls = npz["falls"]
+    all_objectives = npz["objectives"]
+
+    # sort criteria lexicographically
+    criteria = np.concatenate((all_falls[:,np.newaxis], all_objectives), axis=1)
+    lex = np.lexsort(criteria.T[::-1])
+
+    # show "best" result lexicographically
+    if show_best:
+        env = PoppyHumanoidEnv(pb.POSITION_CONTROL, show=True)
+
+        cam = (1.200002670288086,
+            15.999960899353027,
+            -31.799997329711914,
+            (-0.010284600779414177, -0.012256712652742863, 0.14000000059604645))
+        pb.resetDebugVisualizerCamera(*cam)
+        input('.')
+
+        result = run_traj(env, all_samples[lex[0]], num_cycles)
+        positions = result[0]
+        input('.')
+
+        env.close()
+
+        import matplotlib.pyplot as pt
+        pt.plot(positions * 180. / np.pi)
+        pt.show()
+
+    # print results
+    print('falls, objs: progress, lateral, spin, velocity, jerk')
+    print(criteria.round(4))
+    print("sorted:")
+    print(criteria[lex].round(4))
+
