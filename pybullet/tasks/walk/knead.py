b3e3d3567793c2b89955532f1fb5f66e8d83d8ae
garrettkatz <garrett.katz@gmail.com>
2024-12-08 17:34:26 -0500

    multiple steps (but falls after first)

--- pybullet/tasks/walk/knead.py
+++ pybullet/tasks/walk/knead.py
@@ -10,8 +10,10 @@ np.set_printoptions(linewidth=1000)
 
 if __name__ == "__main__":
 
+    num_steps = 2
+
     with open(os.environ["HOME"] + "/poindexter/knead.pkl", "rb") as f: waypoints = pk.load(f)
-    
+
     env = PoppyHumanoidEnv(pb.POSITION_CONTROL, show=True)
 
     env.place_on_ground(waypoints[1])
@@ -20,20 +22,22 @@ if __name__ == "__main__":
     print("current ankle z", current_ankle_z)
     input('.')
 
-    for waypoint in waypoints[2:]:
-        print(env.angle_dict(waypoint))
-        env.track_trajectory([(.2, env.angle_dict(waypoint))])
+    for step in range(num_steps):
+
+        for waypoint in waypoints[2:]:
+            if step % 2 == 1: waypoint = env.mirror_position(waypoint)
+            env.track_trajectory([(.2, env.angle_dict(waypoint))])
+            state = pb.getLinkState(env.robot_id, env.joint_index["l_ankle_y"])
+            current_ankle_z = state[0][2]
+            print("current ankle z", current_ankle_z)
+            # input('.')
+    
+        # settle at last waypoint
+        env.track_trajectory([(10., env.angle_dict(waypoint))])
         state = pb.getLinkState(env.robot_id, env.joint_index["l_ankle_y"])
         current_ankle_z = state[0][2]
-        print("current ankle z", current_ankle_z)
-        # input('.')
-
-    # settle at last waypoint
-    env.track_trajectory([(2., env.angle_dict(waypoints[-1]))])
-    state = pb.getLinkState(env.robot_id, env.joint_index["l_ankle_y"])
-    current_ankle_z = state[0][2]
-    print("current ankle z", current_ankle_z)    
-    input('.')
+        print("current ankle z", current_ankle_z)    
+        input('.')
 
     # ## try straightening legs quickly to achieve slight liftoff ground
     # env.place_on_ground(waypoints[1])
